fc20caaae5d9ee5b6f45cf3bc04d1a24094c7abe
Vikash Singh <vikassingh1999999@gmail.com>
2026-05-21 04:13:33 +0530

    Initialized signin, signup and middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,40 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3000);
+app.post("/signup", (req, res) => {
+	//db call
+
+	res.json({
+		userId: 1230	
+	})
+
+})
+
+app.post("/signin", (req, res) => {
+
+	const userId = 1;
+	const token = jwt.sign({
+		userId
+	}, JWT_SECRET);
+
+	res.json({
+		token
+	})
+
+})
+
+app.post("/room", middleware ,(req, res) => {
+	//dc call
+
+	res.json({
+		roomId: 1230
+	})
+
+})
+
+app.listen(3001);
+
